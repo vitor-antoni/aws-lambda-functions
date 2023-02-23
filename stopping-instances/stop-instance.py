3a48aa3439a65bebdd8045575ee974bb231aa176
Vitor Antoni <v.silva.antoni@gmail.com>
2023-02-23 14:10:12 -0300

    Create stop-instance.py
    Arquivo de configuração da função Lambda para desligar a instância

---
+++ stopping-instances/stop-instance.py
@@ -0,0 +1,18 @@
+import json, boto3
+
+def lambda_handler(event, context):
+    region = '<region>'
+    ec2 = boto3.client('ec2',region_name=region)
+    
+    for record in event['Records']:
+        body = record['body']
+        print('ID da Instancia: ' + body)
+        
+        # Desligando instancia
+        instances = [body]
+        try:
+            ec2.stop_instances(InstanceIds=instances)
+            print('Sucesso para desligar instancia: ' + instances)
+        except Exception as error:
+            print(f'Houve um erro com a função Lambda: {error}')
+            
