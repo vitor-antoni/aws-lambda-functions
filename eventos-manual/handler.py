ce30ed31d8c1807e344decb912dbb9fd520cf553
Vitor Antoni <v.silva.antoni@gmail.com>
2023-08-04 16:25:43 -0300

    Update handler.py

--- eventos-manual/handler.py
+++ eventos-manual/handler.py
@@ -1,5 +1,16 @@
 def lambda_handler(event, context):
-    nome = event["nome"]
-    sobrenome = event["sobrenome"]
+    nome = event["nomeInput"]
+    sobrenome = event["sobrenomeInput"]
+    idade = event["idadeInput"]
 
-    return f"Ola, {nome} {sobrenome}"
+    print(f"Olá, {nome} {sobrenome}. Você tem {idade} anos.")
+
+    # Para popular o espaço de "response" do ambiente de teste
+    # Se não especificar, a função retornará "null" na área de *response*
+    response = {
+        "nome": nome,
+        "sobrenome": sobrenome,
+        "idade": idade
+    }
+
+    return response
